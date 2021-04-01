95410a976aeba454906a8f6fffd9b29907d849da
andcov <andreicovaci123@gmail.com>
2021-04-01 14:50:40 +0300

    Working with no components

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
-const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-const height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+function create_cells() {
+  const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  const height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-const cell_cnt = Math.floor(height / 50) * Math.floor(width * 8 / 10 / 50)
-console.log(`H: ${height} W: ${width}`)
-console.log(cell_cnt)
+  const cell_cnt = Math.floor(height / 50) * Math.floor(width * 8 / 10 / 50);
+  console.log(`H: ${height} W: ${width}`);
+  console.log(cell_cnt);
 
-let cells = [];
-for(let i = 0; i < cell_cnt; i++) {
-  cells.push(
-    { id: i,
-      is_start: false,
-      is_end: false,
-      is_wall: false,
-    });
-} 
+  let cells = [];
+  for(let i = 0; i < cell_cnt; i++) {
+    cells.push(
+      { id: i,
+        is_start: false,
+        is_end: false,
+        is_wall: false,
+      });
+  }
+
+  return cells;
+}
+
+function reset_board() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].is_wall = false;
+  }
+}
+
+
+let cells = create_cells();
 
 cells[1].is_start = true;
-cells[350].is_end = true;
-
-Vue.component('cell', {
-  props: ['is_start', 'is_end', 'is_wall'],
-  template: '<div class="cell" v-bind:class="{start_cell: is_start, end_cell: is_end, wall_cell: is_wall}" v-on:click="is_wall = !is_wall"></div>'
-});
+cells[2].is_end = true;
 
 var container = new Vue({
   el: '.board',
   data: {
-    cells: cells
+    cells: cells,
+    is_placing_walls: false,
+  },
+  methods: {
+    mouse_over: function(cell){
+      if(this.is_placing_walls) {
+        cell.is_wall = !cell.is_wall;
+      }
+    },
+    mouse_down: function(){
+      this.is_placing_walls = true;   
+    },
+    mouse_up: function(){
+      this.is_placing_walls = false;   
+    }
   }
 });
